067389aedbf3d306dc9493183cd838635f363f37
Andres Ballares <mauro.ballares@gmail.com>
2020-12-12 15:23:56 -0500

    answer

--- problems/6.random.js
+++ problems/6.random.js
@@ -5,8 +5,8 @@
  * @returns {number} - a random number between 0 and 1
  * 
  */
-function rand() {
-
+function rand(n) {
+return Math.random(n)
 }
 
 /** 
@@ -15,7 +15,7 @@ function rand() {
  * @returns {number} - A random number greater than 1 and less than 6
  */
 function diceRoll() {
-
+return Math.random(Math.random () * 5 + 1)
 }
 
 /**
@@ -24,8 +24,8 @@ function diceRoll() {
  * @param {number} n - A number that serves as the upper limit of random
  * @returns {number} - A random number betweeen 0 and n
  */
-function randRange() {
-
+function randRange(n) {
+return Math.random(Math.random () * n)
 }
 
 
@@ -37,7 +37,7 @@ function randRange() {
  * @returns {number} - the number 4. don't overthink it.
  */
 function totallyFairDiceRoll() {
-
+return 4
 }
 
 module.exports = {
